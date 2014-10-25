229f6c18b0979a283ba5d77e41c5eca73cc7398a
Jack <jackeagle102@gmail.com>
2014-10-25 14:59:50 +0530

    Liquid Support

--- liquid.mk
+++ liquid.mk
@@ -3,12 +3,12 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/liquid/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/armani/full_armani.mk)
 
 PRODUCT_RELEASE_NAME := Redmi 1S
-PRODUCT_NAME := cm_armani
+PRODUCT_NAME := liquid_armani
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
