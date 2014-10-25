93c72814e7fb66227d6541582b12cd2c2d32dede
Jack <jackeagle102@gmail.com>
2014-10-25 15:27:40 +0530

    AICP Support

--- aicp.mk
+++ aicp.mk
@@ -3,12 +3,12 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/configs/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/armani/full_armani.mk)
 
 PRODUCT_RELEASE_NAME := Redmi 1S
-PRODUCT_NAME := cm_armani
+PRODUCT_NAME := aicp_armani
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
